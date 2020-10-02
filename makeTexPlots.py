4f41912a01a8245c4a049d902bf4ffa82098b9e0
Aaron Webb <aaron.f.webb@gmail.com>
2020-10-02 10:21:43 -0500

    add syst, fullR2 sample files

--- makeTexPlots.py
+++ makeTexPlots.py
@@ -4,35 +4,25 @@ Save as .pdf, and write latex file with plots included in plots/<out string> dir
 Usage: python3.6 featurePlots.py <input dataset.csv> <out string>
 '''
 
-import pandas
-import xgboost as xgb
-import pickle
-import pandas as pd
-import sys
-import matplotlib
-matplotlib.use('agg')
-import matplotlib.pyplot as plt
-import numpy as np
-from sklearn import preprocessing
-from namePlot import name
-#get_ipython().run_line_magic('matplotlib', 'inline')
+import sys, os
 
-inFile = sys.argv[1]
+inPath = sys.argv[1]
 outDir = sys.argv[2]
-
-inDF = pd.read_csv(inFile, nrows=1e3)
-inDF = inDF.dropna()
+fileList = os.listdir(inPath)
+fileList = [inPath + '/' + x for x in fileList]
 
 texfile = open('texPlots_'+outDir+".tex", "w")
 
-print('\\documentclass[hyperref={pdfpagelayout=SinglePage}]{beamer}\\usetheme{Warsaw}\\usepackage{euler}\\usepack\age{pgf}\\usecolortheme{crane}\\usefonttheme{serif}\\useoutertheme{infolines}\\usepackage{epstopdf}\\usepackage{xcolor}\\usep\ackage{multicol}\\title{Plots}', file=texfile)
+print('\\documentclass[hyperref={pdfpagelayout=SinglePage}]{beamer}\\usetheme{Warsaw}\\usepackage{euler}\\usepackage{pgf}\\usecolortheme{crane}\\usefonttheme{serif}\\useoutertheme{infolines}\\usepackage{epstopdf}\\usepackage{xcolor}\\usepackage{multicol}\\title{Plots}', file=texfile)
 print('\\begin{document}', file=texfile)
 
 icount = 1
-for c in inDF:
+for f in fileList:
+    if "Summary" in f:
+        continue
     if icount % 6 == 1:
         print('\\frame{', file=texfile)
-    print(r'\includegraphics[width=.47\linewidth]{%s}' % ('plots/'+outDir+'/features/'+c+".pdf") + ('%'if (icount % 3 != 0) else r'\\'), file=texfile)
+    print(r'\includegraphics[width=.29\linewidth]{%s}' % (f) + ('%'if (icount % 3 != 0) else r'\\'), file=texfile)
     if icount % 6 == 0:
         print('}\n', file=texfile)
     icount += 1
